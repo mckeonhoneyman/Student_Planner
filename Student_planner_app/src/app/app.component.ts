8f011964281ca44c8a3673b21f88322cc38fc55f
osailer <osailer13@gmail.com>
2025-04-27 15:41:43 -0500

    Fixed calendar not displaying correctly

--- Student_planner_app/src/app/app.component.ts
+++ Student_planner_app/src/app/app.component.ts
@@ -11,8 +11,4 @@ import { CalendarComponent } from './calendar/calendar/calendar.component';
 })
 export class AppComponent {
   title = 'Student_planner_app';
-  meetings = {
-    '2024-04-05': ['Dring Coffee', 'Learn React', 'Sleep'],
-    '2024-04-06': ['Dring Coffee', 'Learn Angular', 'Sleep'],
-  }
 }
