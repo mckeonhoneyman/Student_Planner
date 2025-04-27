d67b3602e766cc455fac3a4f2759d1494ee62a9b
osailer <osailer13@gmail.com>
2025-04-27 14:44:58 -0500

    Basic Calendar Functionality

--- Student_planner_app/src/app/app.component.ts
+++ Student_planner_app/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar/calendar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Student_planner_app';
+  meetings = {
+    '2024-04-05': ['Dring Coffee', 'Learn React', 'Sleep'],
+    '2024-04-06': ['Dring Coffee', 'Learn Angular', 'Sleep'],
+  }
 }
